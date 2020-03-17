54f476f16bf927036749e22d64941ba2769068af
Toshihiro.Morita <tomonak@gd6.so-net.ne.jp>
2020-03-17 22:31:34 +0900

    flutter_player

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:audiofileplayer/audiofileplayer.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Audio _audio = Audio.load('assets/tohokami8x.m4a', playInBackground: true);
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          FlatButton(
+            key: null,
+            onPressed: onButtonPlay,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                "Play",
+              ),
+            ),
+          ),
+          FlatButton(
+            key: null,
+            onPressed: onButtonPause,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                "Pause",
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void onButtonPlay() {
+    _audio.play();
+  }
+
+  void onButtonPause() {
+    _audio.pause();
+  }
+}
